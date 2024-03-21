f82aaddf37fbb12054b6a7c7f051b9e9db88474f
nsakalkale <sakalkalenimish2004@gmail.com>
2024-03-21 15:56:02 +0530

    services

--- src/components/Services.js
+++ src/components/Services.js
@@ -17,9 +17,9 @@ export default function Services() {
           <span>Let's Go !!</span>
         </div>
         <div className="row">
-          <div className="col-sm-12 bg-white shadow-sm rounded">
+          <div className="col-sm-12 bg-white  rounded">
             <div className="p-4">
-              <div className="row align-items-center">
+              <div className="row">
                 <div className="col-sm-6">
                   <video
                     className="w-100"
@@ -32,82 +32,50 @@ export default function Services() {
                   </video>
                   <br />
                   <br />
-                  <p>
-                    Meet <b>Mr. Steve</b>, a captivating historian with an
-                    infectious passion for the past. With a tweed jacket and a
-                    well-worn leather satchel slung over his shoulder, Mr. Steve
-                    embodies the essence of a seasoned educator dedicated to
-                    unraveling the mysteries of history. As he strides into the
-                    classroom, his eyes alight with enthusiasm, students can't
-                    help but be drawn to his magnetic presence. With a warm
-                    smile and a twinkle in his eye, he welcomes his eager
-                    pupils, ready to embark on another journey through the
-                    annals of time.
-                  </p>
                 </div>
+
                 <div className="col-sm-6">
                   <h2>
                     <img src={mentor} width={50} alt="Mentor" />
-                    &nbsp; "Synapse Scholar: Your Virtual Mentor"
+                    &nbsp; <b>"Synapse Scholar: Your Virtual Mentor"</b>
                   </h2>
+                  <br />
                   <p>
                     "Synapse Scholar: Your Virtual Mentor" revolutionizes
-                    learning through an innovative Unreal Engine application.
-                    Seamlessly integrating advanced AI technology, it offers
-                    users a transformative educational experience. The AI
-                    teacher within the app serves as a personalized mentor,
-                    guiding learners through their educational journey. With its
-                    cutting-edge features, Synapse Scholar adapts to each user's
-                    unique learning style and pace, ensuring maximum
-                    comprehension and retention. Whether users are studying
-                    complex subjects or seeking practical advice, the AI teacher
-                    delivers tailored explanations and support in real-time. The
-                    application's immersive environment enhances engagement,
-                    allowing users to interact with educational content in a
-                    dynamic and interactive way. From virtual lectures to
-                    interactive simulations, Synapse Scholar provides a diverse
-                    range of learning experiences to cater to various
-                    preferences and needs. Moreover, Synapse Scholar fosters
-                    collaboration among learners by facilitating virtual
-                    classrooms and group discussions. Through shared experiences
-                    and peer interaction, users can deepen their understanding
-                    of topics and benefit from collective knowledge. In summary,
-                    Synapse Scholar empowers learners to reach their full
-                    potential by providing personalized guidance, immersive
-                    experiences, and collaborative learning opportunities—all
-                    powered by Unreal Engine technology and an advanced AI
-                    teacher. It's not just an educational tool; it's a
-                    transformative mentorship platform for lifelong learners.
+                    learning. This Unreal Engine app seamlessly integrates AI
+                    for a personalized learning experience. Your AI mentor
+                    adapts to your pace and style, offering real-time
+                    explanations and support. Immerse yourself in virtual
+                    lectures, simulations, and collaborate with others in
+                    virtual classrooms. Synapse Scholar: Your future of
+                    learning, powered by AI and Unreal Engine.
                   </p>
                 </div>
               </div>
+              <p>
+                Captivating historian <b>Mr. Steve</b> ignites a passion for the
+                past. His tweed jacket and worn satchel hint at a life spent
+                unraveling history's mysteries. Enthusiasm gleams in his eyes as
+                he welcomes students to a journey through time.
+              </p>
             </div>
           </div>
         </div>
         <div className="p-4">
-          <div className="row align-items-center">
-            <div className="col-sm-6">
+          <div className="row">
+            <div className="col-sm-6 mt-5">
               <h2>
                 <img src={langai} width={50} alt="Mentor" />
-                &nbsp; "LangAI: Personal Language Mentor"
+                &nbsp; <b>"LangAI: Personal Language Mentor"</b>
               </h2>
               <p>
-                Languishing in language limbo? LangAI, your personal AI mentor,
-                is here to rescue you! Forget grammar mazes and pronunciation
-                pitfalls. LangAI not only corrects your writing and speech, but
-                also acts as a patient guide, explaining the "why" behind the
-                corrections. It adapts to your level, whether you're a beginner
-                tripping over "hello" or an intermediate learner seeking to
-                finesse your fluency. Imagine practicing with a supportive AI
-                partner who gently corrects your "bonjour" butchering and
-                suggests more eloquent expressions. LangAI fosters a safe space
-                for experimentation and learning, replacing fear with
-                confidence. Through engaging activities and personalized
-                feedback, you'll transform from a language struggler to a
-                confident communicator. LangAI isn't just a correction tool;
-                it's a springboard to fluency. Let LangAI become your
-                indispensable guide, unlocking your linguistic potential, one
-                step (or phrase) at a time.
+                Struggling with a new language? LangAI, your AI mentor, empowers
+                you to conquer grammar, pronunciation, and fluency. Receive
+                personalized feedback and clear explanations to bridge the gap
+                between beginner stumbles and confident communication. LangAI
+                creates a supportive learning environment. Practice
+                conversations, refine pronunciation, and unlock your linguistic
+                potential.
               </p>
               <div className="text-center">
                 <NavLink to="/langai">
