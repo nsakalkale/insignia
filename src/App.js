327b6563acd4541e9505833f84cc34e394db48d7
nsakalkale <sakalkalenimish2004@gmail.com>
2024-03-23 03:10:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Err from "./components/Err";
 import Main from "./components/Main";
@@ -6,26 +6,20 @@ import About from "./components/About";
 import OurTeam from "./components/OurTeam";
 import Services from "./components/Services";
 import LangAI from "./components/LangAI";
-
+import ScrollToTop from "./ScrollToTop";
 function App() {
   return (
-    <>
-      <html>
-        <body className="vh-100 bg-light">
-          <BrowserRouter>
-            
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="about" element={<About />} />
-              <Route path="ourteam" element={<OurTeam />} />
-              <Route path="services" element={<Services />} />
-              <Route path="langai" element={<LangAI />} />
-              <Route path="*" element={<Err />} />
-            </Routes>
-          </BrowserRouter>
-        </body>
-      </html>
-    </>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="about" element={<About />} />
+        <Route path="ourteam" element={<OurTeam />} />
+        <Route path="services" element={<Services />} />
+        <Route path="langai" element={<LangAI />} />
+        <Route path="*" element={<Err />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
